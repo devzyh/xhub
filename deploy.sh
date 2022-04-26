39762abe961c000a1e5c334ca048340a78d67c19
devzyh <devzyh@foxmail.com>
2022-04-26 10:21:09 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# git
+git config --global credential.helper store
+git pull origin master
+
+# maven
+mvn clean package
+
+# directory
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+# stop
+echo "Stop App"
+pid=$(cat build/run.pid)
+kill -9 $pid
+echo "Stop Success"
+
+# deploy
+rm -rf build/app.jar
+cp app/target/app*.jar build/app.jar
+
+# start
+cd build
+echo "Start App"
+nohup java -jar app.jar --spring.profiles.active=prod > run.log &
+echo $! > run.pid
+echo "Start Success"
+cd ..
