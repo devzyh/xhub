4bb54edf13d75d0faa784af0b83911aea3b8bfef
devzyh <devzyh@foxmail.com>
2022-05-20 21:57:28 +0800

    notebook

--- admin/src/views/notebook/content/markdown/index.vue
+++ admin/src/views/notebook/content/markdown/index.vue
@@ -1,13 +1,27 @@
 <template>
-
+  <div ref="editor"></div>
 </template>
 
 <script>
+import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core';
+import { nord } from '@milkdown/theme-nord';
+import { commonmark } from '@milkdown/preset-commonmark';
 export default {
-  name: "index"
-}
+  name: 'Editor',
+  props: {
+    msg: String,
+  },
+  mounted() {
+    Editor.make()
+      .config((ctx) => {
+        ctx.set(rootCtx, this.$refs.editor);
+        ctx.set(defaultValueCtx, this.$props.msg);
+      })
+      .use(nord)
+      .use(commonmark)
+      .create();
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style></style>
