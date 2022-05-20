8a6a7ce4a910e2bd10ed6425264d6778755de0b2
devzyh <devzyh@foxmail.com>
2022-05-21 01:08:26 +0800

    vditor

--- admin/src/views/notebook/content/markdown/index.vue
+++ admin/src/views/notebook/content/markdown/index.vue
@@ -1,27 +1,113 @@
 <template>
-  <div ref="editor"></div>
+  <div class="app-container">
+    <div id="vditor" name="markdown"></div>
+  </div>
 </template>
-
 <script>
-import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core';
-import { nord } from '@milkdown/theme-nord';
-import { commonmark } from '@milkdown/preset-commonmark';
+import Vditor from "vditor"
+import "vditor/dist/index.css"
+
 export default {
-  name: 'Editor',
-  props: {
-    msg: String,
+  data() {
+    return {
+      vditor: ""
+    }
   },
   mounted() {
-    Editor.make()
-      .config((ctx) => {
-        ctx.set(rootCtx, this.$refs.editor);
-        ctx.set(defaultValueCtx, this.$props.msg);
-      })
-      .use(nord)
-      .use(commonmark)
-      .create();
+    // 参考：https://ld246.com/article/1549638745630
+    this.vditor = new Vditor("vditor", {
+      cdn: 'https://unpkg.com/vditor@3.8.14/',
+      height: "83vh",
+      lang: "zh_CN",
+      mode: "ir",
+      theme: "classic",
+      icon: "ant",
+      toolbar: [
+        {
+          hotkey: '⇧⌘S',
+          name: 'sponsor',
+          tipPosition: 's',
+          tip: '成为赞助者',
+          className: 'right',
+          click() {
+            alert('捐赠地址：https://ld246.com/sponsor')
+          },
+        },
+        // "emoji",
+        "headings",
+        "bold",
+        "italic",
+        "strike",
+        "link",
+        "|",
+        "list",
+        "ordered-list",
+        "check",
+        "outdent",
+        "indent",
+        "|",
+        "quote",
+        "line",
+        "code",
+        "inline-code",
+        "insert-before",
+        "insert-after",
+        "|",
+        "upload",
+        // "record",
+        "table",
+        "|",
+        "undo",
+        "redo",
+        "|",
+        // "fullscreen",
+        // "edit-mode",
+        {
+          name: "more",
+          toolbar: [
+            "both",
+            "code-theme",
+            "content-theme",
+            "export",
+            "outline",
+            "preview",
+            // "devtools",
+            // "info",
+            // "help",
+          ],
+        },
+      ],
+      toolbarConfig: {
+        hide: false,
+        pin: false
+      },
+      outline: {
+        enable: true
+      },
+      counter: {
+        enable: true
+      },
+      cache: {
+        enable: false
+      },
+      preview: {
+        hljs: {
+          enable: true,
+          style: "monokailight",
+          lineNumber: true
+        },
+        markdown: {
+          codeBlockPreview: false,
+        }
+      },
+      after: () => {
+        this.vditor.setValue("");
+      }
+    })
   },
-};
+  methods: {}
+}
 </script>
 
-<style></style>
+<style>
+</style>
