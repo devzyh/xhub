984a273367c0c02096c10790b41b732f30528695
devzyh <devzyh@foxmail.com>
2022-05-21 13:01:57 +0800

    editor

--- admin/src/views/notebook/content/editor/index.vue
+++ admin/src/views/notebook/content/editor/index.vue
@@ -1,38 +1,49 @@
 <template>
   <div class="app-container">
-    <div id="vditor" name="markdown"></div>
+    <el-form :model="form" ref="queryForm" size="small">
+      <el-form-item prop="title">
+        <el-input v-model="form.title"
+                  style="width: calc(100% - 100px)"
+                  placeholder="请输入笔记标题">
+        </el-input>
+        <el-button
+          style="margin-left: 10px;width: 90px"
+          type="primary"
+          @click="handleSave">保存
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <div id="vditor" name="vditor"></div>
   </div>
 </template>
+
 <script>
 import Vditor from "vditor"
 import "vditor/dist/index.css"
+import {getContent, updateContent} from "@/api/notebook/content";
 
 export default {
+  name: "Editor",
   data() {
     return {
-      vditor: ""
+      vditor: "",
+      form: {
+        id: 1,//undefined,
+        title: undefined,
+        content: undefined
+      }
     }
   },
   mounted() {
     // 参考：https://ld246.com/article/1549638745630
     this.vditor = new Vditor("vditor", {
       cdn: 'https://unpkg.com/vditor@3.8.14/',
-      height: "83vh",
+      height: "81vh",
       lang: "zh_CN",
       mode: "ir",
       theme: "classic",
       icon: "ant",
       toolbar: [
-        {
-          hotkey: '⇧⌘S',
-          name: 'sponsor',
-          tipPosition: 's',
-          tip: '成为赞助者',
-          className: 'right',
-          click() {
-            alert('捐赠地址：https://ld246.com/sponsor')
-          },
-        },
         // "emoji",
         "headings",
         "bold",
@@ -101,11 +112,17 @@ export default {
         }
       },
       after: () => {
-        this.vditor.setValue("");
+        getContent(this.form.id).then(response => {
+          this.form = response.data;
+          this.vditor.setValue(this.form.content, true)
+        });
       }
     })
   },
-  methods: {}
+  methods: {
+    handleSave() {
+    }
+  }
 }
 </script>
 
