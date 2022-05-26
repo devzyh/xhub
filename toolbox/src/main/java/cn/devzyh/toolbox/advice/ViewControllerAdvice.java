bd55d45d7408cd900595a7a94a26cefe09ef2c12
devzyh <devzyh@foxmail.com>
2022-05-26 15:24:30 +0800

    error

--- toolbox/src/main/java/cn/devzyh/toolbox/advice/ViewControllerAdvice.java
+++ toolbox/src/main/java/cn/devzyh/toolbox/advice/ViewControllerAdvice.java
@@ -3,7 +3,12 @@ package cn.devzyh.toolbox.advice;
 import cn.devzyh.toolbox.utils.ViewModelUtils;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class ViewControllerAdvice {
@@ -16,6 +21,22 @@ public class ViewControllerAdvice {
     @ModelAttribute
     public void addAttributes(Model model) {
         ViewModelUtils.loadGlobal(model);
+    }
+
+    /**
+     * 统一异常处理
+     *
+     * @param request
+     * @param e
+     * @return
+     * @throws Exception
+     */
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHander(Exception e) throws Exception {
+        e.printStackTrace();
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("error");
+        return mv;
     }
 
 }
