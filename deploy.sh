5106b39c8ff9b45323abbbe11b1128f641c5c626
devzyh <devzyh@foxmail.com>
2022-11-03 18:18:32 +0800

    Maven本地打包服务器更新

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
-# git
-git config --global credential.helper store
-git pull origin master
 
-# npm
-#cd xhub-admin
-#npm run build:prod
-#cd ..
-#echo "Vue Success"
-
-# maven
-mvn clean package
-
-# directory
-if [ ! -d "build" ]; then
-  mkdir build
-fi
+# download
+echo "Download"
+wget http://cdn.devzyh.cn/xhub/xhub.jar -O xhub.latest.jar
 
 # stop
 echo "Stop App"
-pid=$(cat build/run.pid)
+pid=$(cat run.pid)
 kill -9 $pid
 echo "Stop Success"
 
 # deploy
-rm -rf build/xhub.jar
-cp xhub-app/target/xhub*.jar build/xhub.jar
+rm -rf xhub.jar
+mv xhub.latest.jar xhub.jar
 
 # start
-cd build
 echo "Start App"
 nohup java -jar xhub.jar --spring.profiles.active=prod > run.log 2>&1 &
 echo $! > run.pid
 echo "Start Success"
-cd ..
